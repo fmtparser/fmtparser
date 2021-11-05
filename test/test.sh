54df10666cbc2b9b8d2f7cdb0dfadd77db8e47c0
Maxim Menshikov <maxim@menshikov.org>
2021-11-05 12:20:02 +0300

    [#2] test.sh: fix typo

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,7 @@ export TOP_DIR="$(cd "$(dirname "$(which "$0")")"/.. ; pwd -P)"
 pushd ${TOP_DIR} > /dev/null
 for test in `ls test_suite` ; do
 	./build/test/fmtreader_test < ./test_suite/test_001.txt
-	if [ "$?" != "0" } ] ; then
+	if [ "$?" != "0" ] ; then
 		exit 1
 	fi
 done
