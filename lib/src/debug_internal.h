943f95c9587e71e57bfdf0888af0ae9ae53c66bb
Maxim Menshikov <maxim@menshikov.org>
2021-11-05 11:33:01 +0300

    [#1] Update debug_internal header style

--- lib/src/debug_internal.h
+++ lib/src/debug_internal.h
@@ -1,6 +1,8 @@
-#ifndef FMT_DEBUG_H
+#ifndef FMT_DEBUG_INTERNAL_H
+#define FMT_DEBUG_INTERNAL_H
 
 #ifdef FMT_DEBUG
+/** Perform debug print */
 #define DBG(__fmt, __x...) \
     do {                                                                    \
         fprintf(stderr, "%s: " __fmt "\n", __FUNCTION__, __x);              \
