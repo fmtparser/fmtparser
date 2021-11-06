568127112dd3ab4b2a3115e652455f6626bd3c8e
Maxim Menshikov <maxim@menshikov.org>
2021-11-06 12:27:48 +0300

    [#12] Unit Tests: add a portion of basic tests

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -37,6 +37,116 @@ TEST(Masks, str_ld_s)
     ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
 }
 
+TEST(Masks, percent)
+{
+    const char *str = "Hello, %% world";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_STRING();
+    EXPECT_STR("Hello, ");
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("%%");
+    EXPECT_TYPE(PERCENT);
+    EXPECT_FLAGS(0);
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_STRING();
+    EXPECT_STR(" world");
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
+TEST(Masks, val02x)
+{
+    const char *str = "%02x";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("%02x");
+    EXPECT_TYPE(x);
+    EXPECT_FLAGS(prepend_zero);
+    /* FIXME: width */
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
+
+TEST(Masks, valld)
+{
+    const char *str = "%ld";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("%ld");
+    EXPECT_LEN(l);
+    EXPECT_TYPE(d);
+    EXPECT_FLAGS(0);
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
+TEST(Masks, vallld)
+{
+    const char *str = "%lld";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("%lld");
+    EXPECT_LEN(ll);
+    EXPECT_TYPE(d);
+    EXPECT_FLAGS(0);
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
+
+TEST(Masks, vallli)
+{
+    const char *str = "%lli";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("%lli");
+    EXPECT_LEN(ll);
+    EXPECT_TYPE(i);
+    EXPECT_FLAGS(0);
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
+TEST(Masks, valspacex)
+{
+    const char *str = "% x";
+    fmt_spec    spec;
+    fmt_status  status;
+    const char *tmp = str;
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EOK);
+    EXPECT_PATTERN();
+    EXPECT_STR("% x");
+    EXPECT_TYPE(x);
+    EXPECT_FLAGS(prepend_space);
+
+    ASSERT_EQ(fmt_init_read(&tmp, &spec), FMT_EEOL);
+}
+
 int
 main(int argc, char **argv)
 {
