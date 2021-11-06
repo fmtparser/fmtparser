9b39f6ac2b5d518dda9f6bbeec9af7a3c2598b2b
Maxim Menshikov <maxim@menshikov.org>
2021-11-06 12:06:16 +0300

    [#11] Introduce native bool type

--- lib/include/fmt_bool.h
+++ lib/include/fmt_bool.h
@@ -6,11 +6,11 @@
 #ifndef FMT_BOOL_H
 #define FMT_BOOL_H
 
-/** Internal boolean type */
-typedef enum fmt_bool
-{
-    FMT_FALSE = 0,  /**< FALSE */
-    FMT_TRUE,       /**< TRUE */
-} fmt_bool;
+#include <stdbool.h>
+
+typedef bool fmt_bool;
+
+#define FMT_FALSE false
+#define FMT_TRUE true
 
 #endif
