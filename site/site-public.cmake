23bf866505776b245aa558c684bdaa89ef645dcc
Maxim Menshikov <maxim@menshikov.org>
2021-11-06 19:12:04 +0300

    [#8] Add site definition

---
+++ site/site-public.cmake
@@ -0,0 +1,6 @@
+# Project's public site
+# Author: Maxim Menshikov (maxim@menshikov.org)
+cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
+
+set(SITE_REPO_SPHINX_THEME              "https://github.com/maximmenshikov/sphinx_redactor_theme.git")
+set(SITE_URL_DOXYREST_PREBUILT_LINUX    "https://opensource.interpretica.io/prebuilt/doxyrest-2.1.0-linux-amd64.tar.xz")
